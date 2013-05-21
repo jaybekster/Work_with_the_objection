e84ceb3c11474011856f12ac47b796aa03960f46
jaybekster <jaybekster@gmail.com>
2013-05-21 18:21:06 +0400

    message

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,26 +1,16 @@
-var person={id: 1,
-		name: "name",
-		loyalty: 4,
-		//objections_list: [2,1,3,4],
-		objections_list: [1,2,3,4],
-		introduction_text: "intro_text",
-		closing_text: "closing_text",
-		final_questions: [1,2,3,4],
-		right_final_question: 1,
-		right_closing_text: "right_closing_text",
-		wrong_closing_text: "wrong_closing_text"
-	}
+var person=data.clients[0]
 
-var app = angular.module('ng').controller('Person', function($scope, $element) {
+var app = angular.module('ng').controller('Persons', function($scope, $element) {
 	$scope.person = person;
-	$scope.person.objections_list = [{
-		id:1,
-		text: "text1"
-	}, {
-		id:2,
-		text: "text2"
-	}, {
-		id:3,
-		text: "text3"
-	}]
+	$scope.person.objections = data.objections.filter(function(obj, i) {
+			return person.objections_list.indexOf(obj.id)>-1
+		})
+	console.log($scope.person)
+})
+
+
+
+
+app.controller("Questions", function($scope) {
+	$scope.questions = data.questions;
 })
