36ffa33715e85d71a7f729f40431c719a8951b50
jaybekster <jaybekster@gmail.com>
2013-06-03 18:31:12 +0400

    some fixed

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,6 +1,14 @@
 var person=data.clients[0]
 
-var app = angular.module('ng').controller('Persons', function($scope, $element) {
+var app = angular.module('ng')
+
+app.controller('Persons', function($scope, $element) {
+	function updateObjection(objections) {
+		var objections = objections || data.objections;
+		return objections.filter(function(obj, i) {
+			return $scope.objections_list[obj.id]
+		})
+	}
 	$scope.id = person.id;
 	$scope.name = person.name;
 	$scope.objections_list = (function() {
@@ -10,9 +18,7 @@ var app = angular.module('ng').controller('Persons', function($scope, $element) 
 		})
 		return objections_list;
 	})()
-	$scope.objections = data.objections.filter(function(obj, i) {
-			return $scope.objections_list[obj.id]
-	})
+	$scope.objections = updateObjection();
 	$scope.modal = function(event) {
 		$("#modal_dialog").show();
 	}
@@ -20,6 +26,9 @@ var app = angular.module('ng').controller('Persons', function($scope, $element) 
 		$scope.objections = data.objections.filter(function(obj, i) {
 			return newValue[obj.id]
 		})
+	}, true)
+	$scope.$root.$watch("objections", function(newValue, oldValue) {
+		$scope.objections = updateObjection(newValue)
 	}, true)
 }).directive('habra', function() {
 	return {
@@ -37,22 +46,77 @@ var app = angular.module('ng').controller('Persons', function($scope, $element) 
 
 
 app.controller("Questions", function($scope) {
-	function findQuestionById(id) {
+	function findById(id) {
 		var questions = null;
-		if ( questions = $scope.questions.filter(function(obj) {
+		if ( questions = $scope.model.questions.filter(function(obj) {
 			return obj.id===id;
 		}) ) {
 			return questions[0];
 		}
 	}
-	$scope.questions = data.questions;
-	$scope.types = data.settings.question_types;
+	$scope.model = {
+		questions: data.questions,
+		types: data.settings.question_types
+	}
+	$scope.initial = angular.copy($scope.model);
+	$scope.editQuestion = function(question, $e) {
+		// var element = angular.element($.toElement)
+	}
 	// $scope.saveQuestion = function() {
 	// 	var question = findQuestionById($scope.id);
 	// }
+	$scope.cancel = function() {
+		$scope.model = angular.copy($scope.initial);
+	}
+	$scope.delete = function() {
+		if (!$scope.model.questions) return false;
+		$scope.model.questions.splice( findById($scope.model.questions.id), 1 );
+	}
 })
+
 app.controller("Objections", function($scope, $element) {
-	$scope.objections = data.objections;
+	function updateQuestions(questions, objections) {
+		var questions = questions || data.questions;
+		var objections = objections || $scope.objections;
+		objections.forEach(function(obj, i) {
+			obj.questions = [];
+			obj.question_list.forEach(function(id) {
+				obj.questions.push(
+					questions.filter(function(question) {
+						return question.id===id;
+					})[0]
+				)
+			})
+		})
+
+
+	}
+	$scope.model = {
+		objections: data.objections,
+		objection: null,
+		questions: updateQuestions(null, data.objections)
+	}
+	console.log()
+
+	$scope.initial = angular.copy($scope.model)
+	function findById(id) {
+		var result = $scope.model.objections.filter(function(obj,i) {
+			return obj.id===id;
+		})
+		if (result) return result[0];
+	}
+	$scope.delete = function() {
+		if (!$scope.model.objection) return false;
+		$scope.model.objections.splice( findById($scope.model.objection.id), 1 );
+	}
+	$scope.save = function() {
+		if (!$scope.model.objection) return false;
+
+	}
+	$scope.cancel = function() {
+		$scope.model = angular.copy($scope.initial);
+	}
+	$scope.$root.objections = $scope.model.objections;
 })
 
 $(function() {
